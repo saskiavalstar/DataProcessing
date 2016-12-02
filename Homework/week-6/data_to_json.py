9055cdfb0dd95defa7695a1e73fe53d006675b11
saskiavalstar <saskiavalstar@gmail.com>
2016-12-02 23:41:08 +0100

    upload week - 6

---
+++ Homework/week-6/data_to_json.py
@@ -0,0 +1,11 @@
+import csv
+import json
+
+with open('d3line_data.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    dict_list = []
+    for row in reader:
+        dict_list.append(row)
+
+with open('d3line_data.json', 'w') as outfile:
+    json.dump(dict_list, outfile)
